ec75d7f3d78c6fa29347ff234a9c9754da8dadf9
mxcz5359 <156735121+mxcz5359@users.noreply.github.com>
2025-06-20 15:24:45 +0800

    Update 1loads.js

--- 1loads.js
+++ 1loads.js
@@ -1,5 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://fastly.jsdelivr.net/gh/lrplrplrp/live2d@master/";
+const live2d_path = "https://fastly.jsdelivr.net/gh/mxcz5359/live@main/";
 const model_path="https://ghproxy.net/https://raw.githubusercontent.com/lrplrplrp/live2d_api/refs/heads/master/";
 
 // 封装异步加载资源的方法
