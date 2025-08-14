46c86d7c7ba4f3071d60b87df2beca4b91f1420b
mxcz5359 <156735121+mxcz5359@users.noreply.github.com>
2025-08-14 09:27:08 +0800

    Update and rename 4loads.js to loads.js

--- loads.js
+++ loads.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://fastly.jsdelivr.net/gh/mxcz5359/live@main/";
-const model_path="https://ghproxy.net/https://raw.githubusercontent.com/lrplrplrp/live2d_api/refs/heads/master/";
+const live2d_path = "https://live.050002.xyz/";
+const model_path="https://api.050002.xyz/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
@@ -33,7 +33,7 @@ async function loadScriptsInOrder(){
 		loadExternalResource(live2d_path + "live2dcubismcore.min.js", "js"),
 		loadExternalResource("https://libs.baidu.com/jquery/2.0.0/jquery.min.js", "js"),
 		loadExternalResource(live2d_path + "waifu.css", "css"),
-		loadExternalResource("https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/font-awesome/6.0.0/css/all.min.css", "css")
+		loadExternalResource("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css", "css")
 	])
 	await loadExternalResource(live2d_path + "index.min.js", "js");
 	await loadExternalResource(live2d_path + "waifu-tips.js", "js");
